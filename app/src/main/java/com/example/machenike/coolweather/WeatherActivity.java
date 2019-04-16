8b7d5316633345022fdb2154ec74cd0fba4d0c96
MACHENIKE <850167592@qq.com>
2019-04-16 16:30:10 +0800

    完善天气主页面。(1)状态栏颜色与背景图片一致(2)向下滑动可以刷新当前所选县的天气数据。

--- app/src/main/java/com/example/machenike/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/machenike/coolweather/WeatherActivity.java
@@ -2,9 +2,12 @@ package com.example.machenike.coolweather;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -46,6 +49,7 @@ public class WeatherActivity extends AppCompatActivity {
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
     private Button back;
+    private SwipeRefreshLayout swipeRefreshLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +65,7 @@ public class WeatherActivity extends AppCompatActivity {
         }else{
             loadBingPic();
         }
+        setTitle();
         if(weatherstring!=null){
             String nowdata= getIntent().getStringExtra("weather_id");
             Weather weather =Utility.handleWeatherResponse(weatherstring);
@@ -68,14 +73,21 @@ public class WeatherActivity extends AppCompatActivity {
             if(weatherid.equals(nowdata)){
                 showWeatherInfo(weather);
             }else{
+                swipeRefreshLayout.setRefreshing(true);
                 requestWeather(nowdata);
             }
         }else{
+            swipeRefreshLayout.setRefreshing(true);
             weatherid = getIntent().getStringExtra("weather_id");
             scroll_view.setVisibility(View.INVISIBLE);
             requestWeather(weatherid);
         }
-
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestWeather(weatherid);
+            }
+        });
     }
     public void requestWeather(final String weatherId){
         String weatherUrl = "http://guolin.tech/api/weather?cityid="
@@ -92,6 +104,7 @@ public class WeatherActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             showWeatherInfo(weather);
+                            swipeRefreshLayout.setRefreshing(false);
                         }
                     });
                 }
@@ -170,6 +183,16 @@ public class WeatherActivity extends AppCompatActivity {
         }
         scroll_view.setVisibility(View.VISIBLE);
     }
+    private void setTitle(){
+        if(Build.VERSION.SDK_INT>=21){
+            View decorView =getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            );
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
     private void initView() {
         bing_pic_img = (ImageView) findViewById(R.id.bing_pic_img);
         title_city_tv = (TextView) findViewById(R.id.title_city_tv);
@@ -185,6 +208,8 @@ public class WeatherActivity extends AppCompatActivity {
         scroll_view = (ScrollView) findViewById(R.id.scroll_view);
         drawer_layout = (DrawerLayout) findViewById(R.id.drawer_layout);
         back = (Button)findViewById(R.id.weather_back_bt);
+        swipeRefreshLayout=(SwipeRefreshLayout)findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
